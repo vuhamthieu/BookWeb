c9a866bc196fc248f5f805207d4cdf1ec18a3dfb
vuhamthieu <vuhamthieu@gmail.com>
2021-12-19 11:39:52 +0700

    ctyhjkl;

--- books webstie/js/script.js
+++ books webstie/js/script.js
@@ -158,3 +158,40 @@ var swiper = new Swiper(".blogs-slider", {
     },
   },
 });
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(a => {
+  a.addEventListener('click', () => {
+    const modal = document.querySelector(a.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(a => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
